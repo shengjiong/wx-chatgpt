7620526f757333226c051293c324a96d45f6c9ba
上嗨彭鱼晏 <35161892+shengjiong@users.noreply.github.com>
2023-02-19 12:12:04 +0800

    Update index.js
    key

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const LIMIT_AI_IMAGE_COUNT = 5;
 const LIMIT_COUNT_RESPONSE = '对不起，因为ChatGPT调用收费，您的免费使用额度已用完~'
 
 const configuration = new Configuration({
-  apiKey: '##your api key##',
+  apiKey: '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',
 });
 
 const openai = new OpenAIApi(configuration);
